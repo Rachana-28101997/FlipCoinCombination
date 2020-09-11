c5f1a6dd4c76bca0ab1aaaf6ae48bfb16ba66c89
Rachana R <rachanarrachu28@gmail.com>
2020-09-11 19:11:31 +0530

    Added : Percentage of singlet is calculated and stored in dictionary

--- flipCoinCombinator.sh
+++ flipCoinCombinator.sh
@@ -1,10 +1,37 @@
 #! /bin/bash 
 echo "Welcome to Flip coin combination"
 
+declare -A headTail
+
+times=0
+
+while [ $times -lt 100 ]
+do
 randomNum=$(( RANDOM % 2  ))
+
+
 if [ $randomNum -eq 1 ]
 then
-        echo "HEAD"
+        val=${headTail["Head"]}
+        val=$(($val+1))
+
+        headTail["Head"]=$val
 else
-        echo "TAIL"
+        val=${headTail["Tail"]}
+        val=$(($val+1))
+
+        headTail["Tail"]=$val
+
 fi
+ times=$(($times+1))
+done
+
+headC=${headTail["Head"]}
+tailC=${headTail["Tail"]}
+
+headPer=$((100*$headC/$times))
+
+tailPer=$((100*$tailC/$times))
+
+echo "Head % is : $headPer"
+echo "Tail % is : $tailPer"
